b341fcb9a3c60e031a40d24b1790a24ace52e734
shaikh ameen shaikh abdullah <imameen36@gmail.com>
2025-07-28 20:05:37 +0530

    ADDED .256.jpg

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -20,9 +20,10 @@ const MangaList = () => {
     m.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+ 
   const getCover = (m) =>
     m.coverFilename
-      ? `https://uploads.mangadex.org/covers/${m.id}/${m.coverFilename}`
+      ? `https://uploads.mangadex.org/covers/${m.id}/${m.coverFilename}.256.jpg`
       : "/placeholder.jpg";
 
   return (
